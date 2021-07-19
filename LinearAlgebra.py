50ee09a1a7a0cd66c2c0fcf3a913fe9748a4e0ce
MohamedGamalElSherbiny <mr80_thewizard@me.com>
2021-07-19 04:58:07 +0200

    Renamed my class Added function to invert 2X2 matrix Added function to get determinant of 2X2 matrix Added function to get minor of matrix Added function to get determinant of square matrix Added TODO list Added an in_class main trial function

--- LinearAlgebra.py
+++ LinearAlgebra.py
@@ -1,7 +1,7 @@
-from copy import deepcopy
 import numpy as np
 
-class SolveLinearEquation:
+
+class LinearAlgebra:
     """
     Solves a system of linear equations using Gaussian Echelon Form
 
@@ -17,17 +17,13 @@ class SolveLinearEquation:
     -------
     solution:        numpy
                      Matrix in the echelon form
-
-    See also:
-    --------
-    https://mathworld.wolfram.com/GaussianElimination.html
-    https://en.wikipedia.org/wiki/Gaussian_elimination
     """
 
     def __init__(self, matrix, target_vector):
         self.matrix = np.array(matrix, dtype=np.float64)
         self.target_vector = np.array(target_vector).reshape(-1, 1)
         self.solution = self._solve_linear_equation()
+        self.det = 0
 
     def __is_square(self):
         """
@@ -107,13 +103,13 @@ class SolveLinearEquation:
         https://mathworld.wolfram.com/GaussianElimination.html
         https://en.wikipedia.org/wiki/Gaussian_elimination
         """
-        matrix_copy = deepcopy(matrix)
+        matrix_copy = np.copy(matrix)
         if matrix[pivot_row, pivot_element] != 1:
             matrix_copy[pivot_row, :] /= matrix[pivot_row, pivot_element]
         elif matrix[pivot_row, pivot_element] == 0 and not checked:
             matrix[-1, :] = matrix[pivot_row, :]
             matrix[pivot_row, :] = matrix_copy[-1, :]
-            SolveLinearEquation.__gaussian_reduction(matrix, pivot_row, target_row, pivot_element, checked=True)
+            LinearAlgebra.__gaussian_reduction(matrix, pivot_row, target_row, pivot_element, checked=True)
         elif checked:
             print("Matrix has no answer")
             return False
@@ -148,11 +144,55 @@ class SolveLinearEquation:
         matrix_solution = self.__matrix_properties()
         if matrix_solution:
             # To concatenate the final vector to the matrix
-            matrix = np.concatenate((self.matrix, self.target_vector), axis=1)
-            for i in range(len(matrix) - 1):
-                matrix = self.__gaussian_reduction(matrix, pivot_row=i, target_row=i + 1, pivot_element=i)
-            for row in range(len(matrix)):
-                matrix[row, :] /= matrix[row, row]
-            return matrix
+            concatenated_matrix = np.concatenate((self.matrix, self.target_vector), axis=1)
+            for i in range(len(concatenated_matrix) - 1):
+                concatenated_matrix = self.__gaussian_reduction(concatenated_matrix, pivot_row=i, target_row=i + 1,
+                                                                pivot_element=i)
+            for row in range(len(concatenated_matrix)):
+                concatenated_matrix[row, :] /= concatenated_matrix[row, row]
+            return concatenated_matrix
         else:
             "There is no solution to this matrix.\nOr this matrix has infinite number of solutions."
+
+    def det(self):
+        self.det = self.matrix[0][0] * self.matrix[1][1] - self.matrix[0][1] * self.matrix[1][0]
+
+    def __interchange_matrix(self):
+        inverted_matrix = np.array([[self.matrix[1][1], -1 * self.matrix[0][1]],
+                                    [-1 * self.matrix[1][0], self.matrix[0][0]]])
+        return inverted_matrix
+
+    def inverse_matrix(self):
+        return (1 / self.det) * self.__interchange_matrix()
+
+    # TODO: Coding a function that checks if a 3x3 matrix is invertible
+    # TODO: Coding a function that generates the transpose of a 3x3 matrix
+    # TODO: Coding a function that generates the matrix of minors of a 3x3 matrix
+    # TODO: Coding a function that generates the matrix of cofactors of a 3x3 matrix
+    # TODO: Coding a function that generates the inverse of a 3x3 matrix if it exists
+
+    # def minor(arr, i, j):
+    #     c = arr[:]
+    #     c = np.delete(c, (i), axis=0)  # deleting the row containing the matrix minor from the original matrix copy
+    #     matrix = [np.delete(row, (j), axis=0) for row in
+    #               (c)]  # Looping on each row and deleting the column element below the minor
+    #     print(matrix)
+    #     return matrix
+    #
+    # def det(arr):
+    #     n = len(arr)
+    #     if n == 1: return arr[0][0]
+    #     if n == 2: return arr[0][0] * arr[1][1] - arr[0][1] * arr[1][0]
+    #     sum = 0
+    #     for i in range(0, n):  # looping on columns of the first row (minor values)
+    #         m = minor(arr, 0, i)  # Calculating the minor's corresponding cofactor matrix
+    #         # print("minor of 0 and",i, "is:", m)
+    #         sum = sum + ((-1) ** i) * arr[0][i] * det(
+    #             m)  # summing the product of the minor and the "signed" determinant of the cofactor
+    #     return sum
+
+if __name__ == "__main__":
+    equations = [[1.5, 5.75, 2.6], [1, 1, 1], [0, 1, -1]]
+    answers = [589.5, 200, -20]
+    solveLinearEquation = LinearAlgebra(equations, answers)
+    print(solveLinearEquation.solution)
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from SolveLinearEquation import SolveLinearEquation
+from LinearAlgebra import LinearAlgebra
 
-matrix = [[1.5, 5.75, 2.6], [1, 1, 1], [0, 1, -1]]
-target_vector = [589.5, 200, -20]
-solveLinearEquation = SolveLinearEquation(matrix, target_vector)
-print(solveLinearEquation.solution)
+equations = [[1.5, 5.75, 2.6], [1, 1, 1], [0, 1, -1]]
+answer = [589.5, 200, -20]
+linearAlgebra = LinearAlgebra(equations, answer)
+print(linearAlgebra.solution)
